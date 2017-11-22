4dc319cf8e99f04c4f8f9b8faaa7e454603941e6
rockbyo5 <rockbyo5@gmail.com>
2017-11-21 20:13:25 -0500

    flaskHW update

--- Python_Websites/flaskHW/app/views.py
+++ Python_Websites/flaskHW/app/views.py
@@ -3,25 +3,53 @@ from flask import render_template
 from app import app
 import os
 
+
 @app.route('/')
 @app.route('/index')
-@app.route('/media.imgs')
 def index():
-	filenames = []
-	filepaths = []
-	for file in os.listdir('./app/static/imgs'):
-		filename = os.fsdecode(file)
-		if filename.endswith(".jpg") or filename.endswith(".png"):
-			filenames.append(filename)
-			filepaths.append('static/imgs/'+filename)
-		else:
-			continue
-	return render_template('index.html',
-							title='Home',
-							filenames=filenames,
-							filepaths=filepaths,
-							)
+    return render_template('index.html',
+                           title='Home',
+                           )
 
-@app.route('/download')
-def download():
-	return "This is the download section"
+
+@app.route('/images')
+def images():
+    filenames = []
+    filepaths = []
+    for file in os.listdir('./app/static/imgs'):
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            filenames.append(filename)
+            filepaths.append('static/imgs/' + filename)
+        else:
+            continue
+    return render_template('images.html',
+                           title='Home',
+                           filenames=filenames,
+                           filepaths=filepaths,
+                           )
+
+
+@app.route('/videos')
+def videos():
+    user = "Tommy"
+    filenames = []
+    filepaths = []
+    for file in os.listdir('./app/static/imgs'):
+        filename = os.fsdecode(file)
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            filenames.append(filename)
+            filepaths.append('static/imgs/' + filename)
+        else:
+            continue
+    return render_template('videos.html',
+                           user=user,
+                           title='Home',
+                           filenames=filenames,
+                           filepaths=filepaths,
+                           )
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error404.html')
